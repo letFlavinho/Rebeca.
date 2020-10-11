b9ca04e8a91ce2e2b77105a968a51723dfe09ed5
Flávio Bulhões <68761468+Flavinho1@users.noreply.github.com>
2020-10-11 06:44:14 -0300

    Create painel.js

---
+++ painel.js
@@ -0,0 +1,8 @@
+function abrirPainel (){
+    document.getElementById('painel').className = 'aberto';
+
+}
+function fecharPainel (){
+    document.getElementById('painel').className = 'fechado';
+    
+}
